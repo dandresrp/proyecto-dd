2ec2240f9b900ace9a85b235c9deb698de67b08f
Diego Rios <73723371+dandresrp@users.noreply.github.com>
2025-04-17 12:00:22 -0600

    feat: add environment variables, database connection, and Swagger documentation setup

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import swaggerDocs from './config/swagger.js';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+swaggerDocs(app);
 
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
 
 app.listen(port, () => {
-  console.log(`Servidor ejecutándose en: http://localhost:${port}`);
+  console.log(`API available at http://localhost:${port}`);
 });
