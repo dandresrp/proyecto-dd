09f0ae6ba779a589a389fd52ee066788a0550828
Kaela <akoseguera@unah.hn>
2025-04-18 12:15:52 -0600

    feat: add /api/roles route in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import swaggerDocs from './config/swagger.js';
+import roleRoutes from './routes/roleRoutes.js';
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ swaggerDocs(app);
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
+app.use('/api/roles', roleRoutes);
 
 app.listen(port, () => {
   console.log(`API available at http://localhost:${port}`);
