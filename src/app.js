4087ae84e37bdba444ad4ae97cf30a229d231c83
Kaela <akoseguera@unah.hn>
2025-04-20 07:39:12 -0600

    feat: add /api/estados in app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import swaggerDocs from './config/swagger.js';
 import roleRoutes from './routes/roleRoutes.js';
 import shippingMethodsRoutes from './routes/shippingMethodsRouter.js';
+import stagesRoutes from './routes/stagesRoutes.js'; 
 
 
 dotenv.config();
@@ -17,7 +18,8 @@ app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
 app.use('/api/roles', roleRoutes);
-app.use('/api/metodos-de-envio', shippingMethodsRoutes); 
+app.use('/api/metodos-de-envio', shippingMethodsRoutes);
+app.use('/api/estados', stagesRoutes); 
 
 app.listen(port, () => {
   console.log(`API available at http://localhost:${port}`);
