80be0916406b6aaee6f83c9ec07e024e0a72a81b
Kaela <akoseguera@unah.hn>
2025-04-22 21:52:45 -0600

    feat: add /api/valores in app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import roleRoutes from './routes/roleRoutes.js';
 import shippingMethodsRoutes from './routes/shippingMethodsRouter.js';
 import stagesRoutes from './routes/stagesRoutes.js'; 
 import userRoutes from './routes/userRoutes.js'; 
+import valueRoutes from './routes/valueRoutes.js'; 
 
 
 dotenv.config();
@@ -22,6 +23,7 @@ app.use('/api/roles', roleRoutes);
 app.use('/api/metodos-de-envio', shippingMethodsRoutes);
 app.use('/api/estados', stagesRoutes); 
 app.use('/api/usuarios', userRoutes);
+app.use('/api/valores', valueRoutes);
 
 app.listen(port, () => {
   console.log(`API available at http://localhost:${port}`);
